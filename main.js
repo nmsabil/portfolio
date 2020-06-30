cc6ba9be67b54c5e73922039037e67a8813bf711
Sabil <sabilmdtv1@gmail.com>
2020-06-30 20:36:48 +0100

    projects, contact, resume, footer added

--- main.js
+++ main.js
@@ -43,3 +43,20 @@ $(window).on("load", function () {
 
 
 
+// button to scroll back to top
+
+$(window).on("scroll", function () {
+  $(this).scrollTop() > 100 ? $(".back_top").fadeIn() : $(".back_top").fadeOut()
+}), $(".back_top").click(function () {
+  return $("html, body").animate({
+    scrollTop: 0
+  }, 1e3), !1
+})
+
+// preloader animation
+window.onload = function () {
+  //hide the preloader
+  $("#status").fadeOut(), $("#preloader").delay(200).fadeOut("slow"), $("body").delay(350).css({
+    overflow: "visible"
+  })
+}
