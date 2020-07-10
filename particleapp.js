9b8c7191c8f57a5ae53ae3fcfc0bd369911504e9
Sabil <sabilmdtv1@gmail.com>
2020-07-10 19:00:44 +0100

    fixed scroll button, optimised images for faster load time, changed color scheme and added mailto and tel for quicker contact

--- particleapp.js
+++ particleapp.js
@@ -2,7 +2,7 @@ particlesJS('home',
   {
     "particles": {
       "number": {
-        "value": 80,
+        "value": 100,
         "density": {
           "enable": true,
           "value_area": 800
